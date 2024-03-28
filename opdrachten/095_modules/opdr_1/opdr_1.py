6080ee5022fd6845ddcd0f0aab79c8b1b6b051b0
Joshua8031 <97067906@st.deltion.nl>
2024-03-28 15:13:30 +0100

    Github

--- opdrachten/095_modules/opdr_1/opdr_1.py
+++ opdrachten/095_modules/opdr_1/opdr_1.py
@@ -2,26 +2,15 @@
 # Naam student:
 # Group:
 
-programma
-|-- opdr_1.py
-|-- my_modules
-    |-- __init__.py
-    |-- csv.py
 
 
 from my_modules import mijn_csv
 
-data = [
-    ['Naam', 'Leeftijd', 'Stad'],
-    ['Yaya', '50', 'London'],
-    ['Bobby', '45', 'shmurda'],
-    ['Bombo', '20', 'clat'],
-]
+persons = []
+for line in open("test.csv", 'rt'):
+    lst = mijn_csv.sanitize(line)
+    person = mijn_csv.create_person(lst)
+    person = mijn_csv.add_fullname(person)
+    persons.append(person)
 
-
-mijn_csv.schrijf_csv_bestand('data.csv', data)
-print("CSV-bestand geschreven")
-
-gelezen_data = mijn_csv.lees_csv_bestand('data.csv')
-print("Gelezen data uit CSV-bestand:")
-print(gelezen_data)
+mijn_csv.print_persons(persons)
